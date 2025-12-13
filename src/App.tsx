5e903ddcb7c248f15494a62bf0320aa0e4243492
DevWerik <werikanjos21@gmail.com>
2025-12-12 22:01:35 -0300

    feat: add react-helmet-async for improved document head management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import "./global.css";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet defaultTitle="%s | pizza.shop"/>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+
 }
