8fd40598ee799466c77d99084fe8550e1e4a6511
DevWerik <werikanjos21@gmail.com>
2026-01-20 01:58:35 -0300

    feat: add new UI components and integrate form handling with validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Toaster } from  "sonner";
 import "./global.css";
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet defaultTitle="%s | pizza.shop"/>
+      <Toaster richColors  />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,61 @@
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 
+const signInFormSchema = z.object({
+  email: z.email("Digite um e-mail válido"),
+});
+
+type SignInForm = z.infer<typeof signInFormSchema>;
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos um link de acesso para seu e-mail.", {
+        action: {
+        label: "Reenviar",
+        onClick: () => {handleSignIn(data)},
+        },
+      })
+    } catch {
+      toast.error("Erro ao enviar e-mail. Tente novamente mais tarde.");
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="flex h-full flex-col items-center justify-center text-center">
+        <div className="W-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">Entre com sua conta</p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2"></div>
+            <Label htmlFor="email"> Seu e-mail</Label>
+            <Input id="email" type="email" {...register("email")} />
+            <Button disabled={isSubmitting} className="w-full">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
