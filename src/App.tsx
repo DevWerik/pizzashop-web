2e600db346f6ea6c7d667f253b38f73f4d41bd5a
DevWerik <werikanjos21@gmail.com>
2026-01-21 21:57:15 -0300

    feat: add theme provider and toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { Toaster } from  "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 import "./global.css";
 
 export function App() {
   return (
     <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <Helmet defaultTitle="%s | pizza.shop"/>
       <Toaster richColors  />
       <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 
