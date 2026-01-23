ba707f5558b97b6317fa3783f9ca7679ca611e9e
DevWerik <werikanjos21@gmail.com>
2026-01-23 20:46:55 -0300

    feat: add Dialog component and integrate it into OrderDetails and OrderTableRow

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { DialogTrigger, Dialog  } from "@/components/ui/dialog";
+
 import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 // export interface OrderTableRowProps {}
 
@@ -8,10 +11,15 @@ export function OrderTableRow() {
   return (
         <TableRow>
                   <TableCell>
+                    <Dialog>
+                      <DialogTrigger asChild>
                     <Button variant="outline" size="xs">
                       <Search className="h3 w-3" />
                       <span className="sr-only">Detalhes do pedido</span>
                     </Button>
+                    </DialogTrigger>
+                    <OrderDetails />
+                    </Dialog>
                   </TableCell>
                   <TableCell className="font-mono text-xs font-medium">
                     wskerf98wsnfg34
@@ -20,12 +28,6 @@ export function OrderTableRow() {
                     Há 15 minutos
                   </TableCell>
                   <TableCell>
-                    <div className="flex items-center gap-2">
-                      <span className="h-2 w-2 rounded-full bg-slate-400" />
-                      <span className="font-medium text-muted-foreground">
-                        Pendente
-                      </span>
-                    </div>
                   </TableCell>
                   <TableCell className="font-medium">João da Silva</TableCell>
                   <TableCell className="font-medium">R$ 59,90</TableCell>
